5fda54dbecebcba24db916bb6e93f4ca4b05d79e
ariesfun <ariesfun2019@outlook.com>
2023-07-18 21:46:27 +0800

    update json file parse test

--- main.cpp
+++ main.cpp
@@ -109,20 +109,24 @@ int main() {
 
 
     // test08，解析对象(键值对)
-    const std::string &str = "{\"a\":1,\"b\":2,\"c\":false,\"d\":[1,3,9,\"a\", 1.23, null,true, false, 999.000999]}";
-    Json res; 
-    res.parse(str); 
-    std::cout << res.str() << std::endl;
+    // const std::string &str = "{\"a\":1,\"b\":2,\"c\":false,\"d\":[1,3,9,\"a\", 1.23, null,true, false, 999.000999]}";
+    // Json res; 
+    // res.parse(str); 
+    // std::cout << res.str() << std::endl;
 
 
     // test09，解析json文件
-    // std::ifstream fin("./json_test.json"); // 打开文件
-    // std::stringstream ss;
-    // ss << fin.rdbuf(); // 读取文件
-    // const std::string &str1 = ss.str(); // stringstream()的str()方法，转为字符串
-    // Json res1;
-    // res1.parse(str1);
-    // std::cout << res1.str() << std::endl;
+    std::ifstream fin("./json_test.json"); // 打开文件
+    std::stringstream ss;
+    ss << fin.rdbuf(); // 读取文件
+    const std::string &str1 = ss.str(); // stringstream()的str()方法，转为字符串
+    Json res1;
+    res1.parse(str1);
+    std::cout << res1.str() << std::endl;
+
+    std::string b_username = res1["data"]["uname"];
+    int b_level = res1["data"]["level_info"]["current_level"];
+    std::cout << "your bilibili username: " << b_username << '\n' << "current level: " << b_level << std::endl; 
 
     std::cout << "Debug Test" << std::endl;
     return 0;
